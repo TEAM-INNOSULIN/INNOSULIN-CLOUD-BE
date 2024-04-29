1edd2553d075ff0851dbeca7b2812182e379e775
judemin <judemin2087@naver.com>
2024-04-29 21:20:11 +0900

    ADD : Add putBloodSuger API

--- routes/user.js
+++ routes/user.js
@@ -27,7 +27,27 @@ router.post('/addUser', async (req, res, next) => {
         console.error(error);
         return next(error);
     }
+});
 
+router.put('/putBloodSuger', async (req, res, next) => {
+    try {
+        const { refreshToken, value } = req.body;
+
+        const user = await User.findOne({ refreshToken: refreshToken });
+        if (!user)
+            return res.status(404).json({ status: 'fail', message: 'User not found' });
+
+        // BloodSuger 배열에 새로운 데이터 추가
+        user.bloodSuger.push({ date: new Date(), value });
+
+        // 업데이트된 User 저장
+        await user.save();
+
+        return res.status(200).json({ status: 'success', message: 'Blood sugar data added successfully' });
+    } catch (error) {
+        console.error(error);
+        return next(error);
+    }
 });
 
 module.exports = router;
